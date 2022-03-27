45ce98c568f328ae5dc2ea0f5296a24bc4fa97e4
Pavanjethewad <96859224+Pavanjethewad@users.noreply.github.com>
2022-03-27 17:49:19 +0530

    Add files via upload

---
+++ March/week-2/Assignment-1/FindPermutationInString.java
@@ -0,0 +1,22 @@
+package assignmnet;
+import java.util.Scanner;
+public class FindPermutationInString {
+	static void printPermutation(String s, String ans) {
+        if (s.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+        	char ch = s.charAt(i);
+            String s1 = s.substring(0, i) + s.substring(i + 1);
+            printPermutation(s1, ans + ch);
+        }
+    }
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The String:");
+		String s = sc.nextLine();
+		 printPermutation(s, "");
+	}
+}
